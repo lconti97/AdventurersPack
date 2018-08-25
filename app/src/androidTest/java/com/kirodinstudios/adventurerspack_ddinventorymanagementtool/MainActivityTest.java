86d0d1346693a75871a6272aeab7b1261f0eabeb
Lucas Conti <lconti97@vt.edu>
2018-08-25 16:54:07 -0400

    Test refactors

--- app/src/androidTest/java/com/kirodinstudios/adventurerspack_ddinventorymanagementtool/MainActivityTest.java
+++ app/src/androidTest/java/com/kirodinstudios/adventurerspack_ddinventorymanagementtool/MainActivityTest.java
@@ -1,10 +1,11 @@
 package com.kirodinstudios.adventurerspack_ddinventorymanagementtool;
 
 import com.kirodinstudios.adventurerspack_ddinventorymanagementtool.db.AppDatabase;
+import com.kirodinstudios.adventurerspack_ddinventorymanagementtool.model.ArmorTemplate;
+import com.kirodinstudios.adventurerspack_ddinventorymanagementtool.model.EquipmentTypes;
 import com.kirodinstudios.adventurerspack_ddinventorymanagementtool.model.WeaponTemplate;
 import com.kirodinstudios.adventurerspack_ddinventorymanagementtool.ui.MainActivity;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,7 +18,6 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -31,8 +31,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -46,66 +44,51 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void disableRecyclerViewAnimations() {
-//        EspressoTestUtil.disableAnimations(mActivityRule);
-    }
-
-    @Test
-    public void testCanCreateEquipmentStackAndTemplate() {
-        onView(withId(R.id.add_equipment_stack_button))
-                .perform(click());
-        onView(withId(R.id.equipment_stack_add_fragment_name))
-                .perform(typeText("xq"));
-        onView(withId(R.id.equipment_stack_add_fragment_count))
-                .perform(typeText("5"));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.equipment_stack_add_fragment_done))
-                .perform(click());
-
-        onView(withId(R.id.equipment_stacks_list))
-                .check(matches(isDisplayed()));
-        onView(withText("xq"))
-                .perform(click());
-
-        onView(withId(R.id.equipment_stack_detail_item))
-                .check(matches(isDisplayed()));
-    }
-
     @Test
     public void testCanPopulateFieldsFromArmorTemplate() {
+        ArmorTemplate armorTemplate = new ArmorTemplate("Half plate",
+                "consists of shaped metal plates", 750.0, 40.0,
+                "15 + Dex", "Medium Armor", true,
+                false, null);
+
         onView(withId(R.id.add_equipment_stack_button))
                 .perform(click());
         onView(withId(R.id.equipment_stack_add_fragment_name))
-                .perform(typeText("pl"));
-        onView(withSubstring("Half plate"))
+                .perform(typeText(armorTemplate.getName()));
+        onView(withSubstring(armorTemplate.getName()))
                 .inRoot(isPlatformPopup())
                 .perform(click());
         Espresso.closeSoftKeyboard();
 
-        onView(withId(R.id.equipment_stack_add_fragment_type))
-                .check(matches(allOf(isDisplayed(), withSpinnerText("Armor"))));
-        onView(withId(R.id.equipment_stack_add_fragment_weight))
-                .check(matches(allOf(isDisplayed(), withText("40"))));
-        onView(withId(R.id.equipment_stack_add_fragment_cost))
-                .check(matches(allOf(isDisplayed(), withText("750"))));
-        onView(withId(R.id.armor_class))
-                .check(matches(allOf(isDisplayed(), withSubstring("15 + Dex"))));
-        onView(withId(R.id.armor_category))
-                .check(matches(allOf(isDisplayed(), withSpinnerText("Medium Armor"))));
-        onView(withId(R.id.armor_disadvantage_on_stealth))
-                .check(matches(allOf(isDisplayed(), isChecked())));
-        onView(withId(R.id.armor_requires_minimum_strength))
-                .check(matches(allOf(isDisplayed(), isNotChecked())));
-        onView(withId(R.id.armor_minimum_strength))
-                .check(matches(allOf(isDisplayed(), withText(""))));
-        onView(withId(R.id.equipment_stack_add_fragment_description))
-                .check(matches(allOf(isDisplayed(), withSubstring("consists of shaped metal plates"))));
+        checkArmorTemplateFieldsArePopulated(armorTemplate);
 
         onView(withId(R.id.equipment_stack_add_fragment_done))
                 .perform(click());
-        onView(withSubstring("Half plate"))
+        onView(withSubstring(armorTemplate.getName()))
                 .check(matches(isDisplayed()));
+    }
+
+    private void checkArmorTemplateFieldsArePopulated(ArmorTemplate armorTemplate) {
+        onView(withId(R.id.equipment_stack_add_fragment_type))
+                .check(matches(allOf(isDisplayed(), withSpinnerText(EquipmentTypes.ARMOR))));
+        onView(withId(R.id.equipment_stack_add_fragment_weight))
+                .check(matches(allOf(isDisplayed(), withSubstring(String.valueOf(armorTemplate.getWeightInPounds().intValue())))));
+        onView(withId(R.id.equipment_stack_add_fragment_cost))
+                .check(matches(allOf(isDisplayed(), withSubstring(String.valueOf(armorTemplate.getCostInGp().intValue())))));
+        onView(withId(R.id.armor_class))
+                .check(matches(allOf(isDisplayed(), withSubstring(armorTemplate.getArmorClass()))));
+        onView(withId(R.id.armor_category))
+                .check(matches(allOf(isDisplayed(), withSpinnerText(armorTemplate.getArmorCategory()))));
+        onView(withId(R.id.armor_disadvantage_on_stealth))
+                .check(matches(allOf(isDisplayed(), armorTemplate.getGivesDisadvantageOnStealthChecks() ? isChecked() : isNotChecked())));
+        onView(withId(R.id.armor_requires_minimum_strength))
+                .check(matches(allOf(isDisplayed(), armorTemplate.getRequiresMinimumStrength() ? isChecked() : isNotChecked())));
+        String expectedMinimumStrength = armorTemplate.getMinimumStrength() == null
+                ? "" : String.valueOf(armorTemplate.getMinimumStrength());
+        onView(withId(R.id.armor_minimum_strength))
+                .check(matches(allOf(isDisplayed(), withText(expectedMinimumStrength))));
+        onView(withId(R.id.equipment_stack_add_fragment_description))
+                .check(matches(allOf(isDisplayed(), withSubstring(armorTemplate.getDescription()))));
     }
 
     @Test
@@ -150,29 +133,15 @@ public class MainActivityTest {
                 .perform(click());
         onView(withId(R.id.equipment_stack_add_fragment_name))
                 .perform(typeText(weaponTemplate.getName()));
-        onView(withId(R.id.equipment_stack_add_fragment_count))
-                .perform(typeText(String.valueOf(count)));
         onView(withId(R.id.equipment_stack_add_fragment_type))
                 .perform(click());
-        onView(withText("Weapon"))
+        onView(withText(EquipmentTypes.WEAPON))
                 .inRoot(isPlatformPopup())
                 .perform(click());
-        onView(withId(R.id.equipment_stack_add_fragment_weight))
-                .perform(typeText(String.valueOf(weaponTemplate.getWeightInPounds())));
-        onView(withId(R.id.equipment_stack_add_fragment_cost))
-                .perform(typeText(String.valueOf(weaponTemplate.getCostInGp())));
-        onView(withId(R.id.weapon_damage))
-                .perform(typeText(weaponTemplate.getDamage()));
-        onView(withId(R.id.weapon_properties))
-                .perform(typeText(weaponTemplate.getProperties()));
-        closeSoftKeyboard();
-        onView(withId(R.id.weapon_category))
-                .perform(click());
-        onView(withText("Martial melee weapon"))
-                .inRoot(isPlatformPopup())
-                .perform(click());
-        onView(withId(R.id.equipment_stack_add_fragment_description))
-                .perform(typeText(weaponTemplate.getDescription()));
+
+        onView(withId(R.id.equipment_stack_add_fragment_count))
+                .perform(typeText(String.valueOf(count)));
+        inputWeaponTemplateFields(weaponTemplate);
 
         onView(withId(R.id.equipment_stack_add_fragment_done))
                 .perform(click());
@@ -188,10 +157,34 @@ public class MainActivityTest {
                 .inRoot(isPlatformPopup())
                 .perform(click());
 
-        onView(withId(R.id.equipment_stack_add_fragment_type))
-                .check(matches(withSpinnerText("Weapon")));
+        assertFieldsArePopulated(weaponTemplate);
+    }
+
+    private void inputWeaponTemplateFields(WeaponTemplate weaponTemplate) {
+        onView(withId(R.id.equipment_stack_add_fragment_weight))
+                .perform(typeText(String.valueOf(weaponTemplate.getWeightInPounds())));
+        onView(withId(R.id.equipment_stack_add_fragment_cost))
+                .perform(typeText(String.valueOf(weaponTemplate.getCostInGp())));
+        onView(withId(R.id.weapon_damage))
+                .perform(typeText(weaponTemplate.getDamage()));
+        onView(withId(R.id.weapon_properties))
+                .perform(typeText(weaponTemplate.getProperties()));
+        closeSoftKeyboard();
+        onView(withId(R.id.weapon_category))
+                .perform(click());
+        onView(withText(weaponTemplate.getWeaponType()))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+        onView(withId(R.id.equipment_stack_add_fragment_description))
+                .perform(typeText(weaponTemplate.getDescription()));
+    }
+
+    private void assertFieldsArePopulated(WeaponTemplate weaponTemplate) {
+        int EQUIPMENT_STACK_DEFAULT_COUNT = 1;
         onView(withId(R.id.equipment_stack_add_fragment_count))
-                .check(matches(withText("1")));
+                .check(matches(withText(String.valueOf(EQUIPMENT_STACK_DEFAULT_COUNT))));
+        onView(withId(R.id.equipment_stack_add_fragment_type))
+                .check(matches(withSpinnerText(EquipmentTypes.WEAPON)));
         onView(withId(R.id.equipment_stack_add_fragment_weight))
                 .check(matches(withText(String.valueOf(weaponTemplate.getWeightInPounds()))));
         onView(withId(R.id.equipment_stack_add_fragment_cost))
@@ -201,25 +194,8 @@ public class MainActivityTest {
         onView(withId(R.id.weapon_properties))
                 .check(matches(withText(weaponTemplate.getProperties())));
         onView(withId(R.id.weapon_category))
-                .check(matches(withSpinnerText("Martial melee weapon")));
+                .check(matches(withSpinnerText(weaponTemplate.getWeaponType())));
         onView(withId(R.id.equipment_stack_add_fragment_description))
                 .check(matches(withText(weaponTemplate.getDescription())));
-    }
-
-    @Test
-    public void testWeaponFieldsShowInAddFragment() {
-        onView(withId(R.id.add_equipment_stack_button))
-                .perform(click());
-        onView(withId(R.id.equipment_stack_add_fragment_type))
-                .perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("Weapon")))
-                .perform(click());
-
-        onView(withId(R.id.weapon_damage))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.weapon_properties))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.weapon_category))
-                .check(matches(isDisplayed()));
     }
 }
